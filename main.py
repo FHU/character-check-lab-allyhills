1856032cbdb565ebe31219b423bfb49b7924a250
allyhills <ally.hills@students.fhu.edu>
2024-02-10 12:51:47 -0600

    character-check-lab ally hills

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 #Remove pass and complete the code
 def check_character(word, index):
-   pass
+   char = word[index]
+   char_type = ''
+   if char.isalpha():
+      char_type = 'letter'
+   elif char == ' ':
+      char_type = 'white space'
+   elif char.isdigit():
+      char_type = 'digit' 
+   else:
+      char_type = 'unknown'
+   
+   return char_type
 
-if __name__ == '__main__': 
-    print(check_character('happy birthday', 2))
-    print(check_character('happy birthday', 5))
-    print(check_character('happy birthday 2 you', 15))
-    print(check_character('happy birthday!', 14))
+if __name__ == '__main__':
+   check_character(word, index)
